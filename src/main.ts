d5016263b39bc9a15dce5376037a653c0fef43f6
付柏磊 <1464778543@qq.com>
2024-11-06 16:41:19 +0800

    注册接口及全局安装ValidationPipe管道参数验证

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // 全局安装ValidationPipe管道参数验证-使用 ValidationPipe需要安装所需的依赖包 class-validator 和 class-transformer：
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
